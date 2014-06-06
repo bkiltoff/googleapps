ebe3aaec2f1d7b51e2b5dafa75ed80dd4e4a1fc8
Blake <bkiltoff@gmail.com>
2014-06-06 16:59:35 -0700

    started usersignup

--- bkiltoff-usersignup/main.py
+++ bkiltoff-usersignup/main.py
@@ -14,9 +14,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+"""
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Sign Up</title>
+    <style type="text/css">
+      .label {text-align: right}
+      .error {color: red}
+    </style>
+
+  </head>
+
+  <body>
+    <h2>Signup</h2>
+    <form method="post">
+      <table>
+        <tr>
+          <td class="label">
+            Username
+          </td>
+          <td>
+            <input type="text" name="username" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Password
+          </td>
+          <td>
+            <input type="password" name="password" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Verify Password
+          </td>
+          <td>
+            <input type="password" name="verify" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Email (optional)
+          </td>
+          <td>
+            <input type="text" name="email" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+      </table>
+
+      <input type="submit">
+    </form>
+  </body>
+
+</html>
+"""
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, error="", usr_input_name="",
+                   usr_input_p1="", usr_input_p2="",
+                   usr_input_email=""):
+        self.response.out.write()
+        
     def get(self):
         self.response.write('Hello world!')
 
